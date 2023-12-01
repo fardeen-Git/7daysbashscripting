c3c86c18ccbd9ae9ef9902d7b2a0bcfc94a0e42f
Fardeen khan <113256054+fardeen-Git@users.noreply.github.com>
2023-12-01 21:35:29 +0530

    Update TASK1_COMMENTS.sh

--- DAY1_SOLUTION/TASK1_COMMENTS.sh
+++ DAY1_SOLUTION/TASK1_COMMENTS.sh
@@ -2,5 +2,5 @@
 #is line me mai read ka use karke, challenge naam ke variable me ek info store karrha hu aur ye kaam mai same line me karrha hu using -p message jiska matlab prompt message bhi hota h
 read -p "Tell me about today's challenge:" challenge
 echo $challenge
-echo "is me mai is script ki har line ko explai karha hu using comments"
+echo "is me mai is script ki har line ko explain karha hu using comments"
 #kisi line ko comment banane ke liye hum (#) hashtag ka use karte h
